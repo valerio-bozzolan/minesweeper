ee9d5251ee3aa4efe4647ec205874bd9a74174a9
Valerio Bozzolan <boz@reyboz.it>
2014-03-16 23:29:06 +0100

    First implementation of the game creation and functionality

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,46 +1,83 @@
 /**
  * Terminology:
- * 	N:         N° of <td>
+ * 	N:         N° of <td> per row / N° of <tr> per culumn
  *	side:      pixel of every <td>
  *	field:     the table
  */
 
+/**
+ * Array index legend:
+ * 	0: x
+ *	1: y
+ *
+ * Array content legend:
+ * 	0: Nothing
+ *	1: Bomb
+ */
+
+/**
+ * Globals
+ */
 var field_ID = 'field';
-
 var field = $("#" + field_ID);
+var CSS_side_borders = 2; // px
 
-var game;
+var game = new Array(); // [x][y] = {0, 1};
+var game_max_x;
+var game_max_y;
 
-create_table_from_Nx(20, field);
+/**
+ * Init game
+ */
+game_reset(4, 1); // Fill 20 per x-row, 20 bombs
 
 $("table#" + field_ID + " td").click(function() {
-	var col = $(this).parent().children().index($(this));
-	var row = $(this).parent().parent().children().index($(this).parent());
-	alert(col + " " + row);
+	var x = $(this).parent().children().index($(this));
+	var y = $(this).parent().parent().children().index($(this).parent());
+	if(game[x][y]) {
+		alert("Hai perso");
+	} else {
+		field_indicate_as_flag(x, y);
+	}
 });
 
-function instantiate_table(n_bombs, element) {
-
+function game_reset(Nx, n_bombs) {
+	var candidates = new Array();
+	create_field_from_Nx(Nx);
+	for(var i=0; i<game_max_x; i++) {
+		game[i] = new Array();
+		for(var j=0; j<game_max_y; j++) {
+			game[i][j] = 0; // Reset game
+			candidates.push(new Array(i, j));
+		}
+	}
+	if(n_bombs >= candidates.length) {
+		return false; // There are more bombs than elements
+	}
+	candidates = shuffle(candidates);
+	for(var i=0; i<n_bombs; i++) {
+		game[ candidates[i][0] ][ candidates[i][1] ] = 1; // Element elected as bomb
+	}
 }
 
-function create_table_from_Nx(Nx, element) {
+function create_field_from_Nx(Nx) {
 	var side = float2int(window.innerWidth / Nx);
-	create_table_from_side(side, element);
+	create_field_from_side(side);
 }
 
-function create_table_from_side(side, element) {
-	var Nx = get_Nx_from_side(side);
-	var Ny = get_Ny_from_side(side);
+function create_field_from_side(side) {
 	var table = "";
-	for(var i=0; i<Ny; i++) {
+	game_max_x = get_Nx_from_side(side);
+	game_max_y = get_Ny_from_side(side);
+	for(var i=0; i<game_max_y; i++) {
 		table += "<tr>";
-		for(var j=0; j<Nx; j++) {
+		for(var j=0; j<game_max_x; j++) {
 			table += "<td></td>";
 		}
 		table += "</tr>";
 	}
-	element.html(table);
-	change_CSS_side(side);
+	field.html(table);
+	field_change_CSS_side(side);
 }
 
 function get_Nx_from_side(side) {
@@ -56,7 +93,16 @@ function float2int(value) {
 	return value | 0;
 }
 
-function change_CSS_side(side) {
-	$("td").width(side - 2); // Less CSS borders
-	$("td").height(side - 2); // Less CSS borders
+function field_change_CSS_side(side) {
+	$("td").width(side - CSS_side_borders);
+	$("td").height(side - CSS_side_borders);
 }
+
+function field_indicate_as_flag(x, y) {
+	$("tr").eq(y).find("td").eq(x).css("background", "grey");
+}
+
+function shuffle(o) {
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
