57e8850268c073a219ddfff9e560d3e22d00cabb
Valerio Bozzolan <boz@reyboz.it>
2014-03-17 00:06:40 +0100

    Game works with default options

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -11,8 +11,9 @@
  *	1: y
  *
  * Array content legend:
- * 	0: Nothing
- *	1: Bomb
+ *	2: Flagged
+ * 	1: Nothing
+ *	0: Bomb
  */
 
 /**
@@ -25,39 +26,59 @@ var CSS_side_borders = 2; // px
 var game = new Array(); // [x][y] = {0, 1};
 var game_max_x;
 var game_max_y;
+var game_flags;
+var game_bombs;
 
-/**
- * Init game
- */
-game_reset(4, 1); // Fill 20 per x-row, 20 bombs
+new_game(8, 8);
 
-$("table#" + field_ID + " td").click(function() {
-	var x = $(this).parent().children().index($(this));
-	var y = $(this).parent().parent().children().index($(this).parent());
-	if(game[x][y]) {
-		field_indicate_as_bomb(x, y);
-		alert("Hai perso");
-	} else {
-		field_indicate_as_flag(x, y);
-	}
+$(".play").click(function() {
+	new_game(8, 8);
 });
 
-function game_reset(Nx, n_bombs) {
+function new_game(bombs, Nx) {
+	GUI_clear_table();
+	set_bombs(bombs);
+	game_prepare(Nx);
+	$("table#" + field_ID + " td").click(function() {
+		var x = $(this).parent().children().index($(this));
+		var y = $(this).parent().parent().children().index($(this).parent());
+		switch(game[x][y]) {
+			case 0:
+				GUI_set_bomb(x, y);
+				alert("Hai perso");
+				break;
+			case 1:
+				game[x][y] = 2;
+				increase_flags_counter();
+				GUI_set_flag(x, y);
+				break;
+			case 2:
+				game[x][y] = 1;
+				decrease_flags_counter();
+				GUI_set_nothing(x, y);
+				break;
+		}
+	});
+}
+
+function game_prepare(Nx) {
+	game = new Array();
 	var candidates = new Array();
+	set_flags_counter(0)
 	create_field_from_Nx(Nx);
 	for(var i=0; i<game_max_x; i++) {
 		game[i] = new Array();
 		for(var j=0; j<game_max_y; j++) {
-			game[i][j] = 0; // Reset game
+			game[i][j] = 1; // Reset game
 			candidates.push(new Array(i, j));
 		}
 	}
-	if(n_bombs >= candidates.length) {
+	if(game_bombs >= candidates.length) {
 		return false; // There are more bombs than elements
 	}
 	candidates = shuffle(candidates);
-	for(var i=0; i<n_bombs; i++) {
-		game[ candidates[i][0] ][ candidates[i][1] ] = 1; // Element elected as bomb
+	for(var i=0; i<game_bombs; i++) {
+		game[ candidates[i][0] ][ candidates[i][1] ] = 0; // Element elected as bomb
 	}
 }
 
@@ -78,7 +99,7 @@ function create_field_from_side(side) {
 		table += "</tr>";
 	}
 	field.html(table);
-	field_change_CSS_side(side);
+	GUI_set_side(side);
 }
 
 function get_Nx_from_side(side) {
@@ -94,20 +115,47 @@ function float2int(value) {
 	return value | 0;
 }
 
-function field_change_CSS_side(side) {
+function set_bombs(n) {
+	game_bombs = n;
+	$(".bombs-counter").html(n);
+}
+function set_flags_counter(n) {
+	game_flags = n;
+	$(".flags-counter").html(n);
+}
+function increase_flags_counter() {
+	set_flags_counter(game_flags + 1);
+}
+function decrease_flags_counter() {
+	set_flags_counter(game_flags - 1);
+}
+
+/**
+ * GUI functions
+ */
+function GUI_set_flag(x, y) {
+	$("tr").eq(y).find("td").eq(x).css("background", "grey");
+}
+function GUI_set_bomb(x, y) {
+	$("tr").eq(y).find("td").eq(x).css("background", "red");
+}
+function GUI_set_nothing(x, y) {
+	$("tr").eq(y).find("td").eq(x).css("background", "yellow");
+}
+function GUI_set_side(side) {
 	$("td").width(side - CSS_side_borders);
 	$("td").height(side - CSS_side_borders);
 }
-
-function field_indicate_as_flag(x, y) {
-	$("tr").eq(y).find("td").eq(x).css("background", "grey");
+function GUI_clear_table() {
+	 field.empty();
 }
 
-function field_indicate_as_bomb(x, y) {
-	$("tr").eq(y).find("td").eq(x).css("background", "red");
-}
-
-function shuffle(o) {
-	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	return o;
+/**
+ * Basically functions
+ */
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/array/shuffle [v1.0]
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
 };
